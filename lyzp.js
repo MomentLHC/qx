f4bf2750fa6fed4ba57bfeffc110b64c71fc65f3
MomentLHC <80976379+MomentLHC@users.noreply.github.com>
2025-12-09 20:36:52 +0800

    Update lyzp.js

--- lyzp.js
+++ lyzp.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
 # > 趣乐转谱 (EditScore)
-^https?:\/\/editscore-api\.quthing\.com\/rolls\/score\/(limit|query) url script-response-body https://raw.githubusercontent.com/MomentLHC/qx/refs/heads/ml/lyzp.js
+^https?:\/\/editscore-api\.quthing\.com\/rolls\/score\/(limit|query) url script-response-body https://raw.githubusercontent.com/MomentLHC/qx/ml/lyzp.js
 
 
 [mitm]
