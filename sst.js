dd7c7318c8665e9c63b9e1d44eccf393e006c427
MomentLHC <80976379+MomentLHC@users.noreply.github.com>
2025-12-09 21:56:37 +0800

    Update sst.js

--- sst.js
+++ sst.js
@@ -12,43 +12,18 @@ hostname = api.butingxue.net
 */
 
 var body = $response.body;
-var url = $request.url;
+var obj = JSON.parse(body);
 
-try {
-    var obj = JSON.parse(body);
-
-    if (obj.data) {
-        // ---------------- 核心破解逻辑 ----------------
-        
-        // 1. 修改 VIP 状态码
-        // 原始是 -1 (int)，修改为 2 (int)，注意类型不要用字符串
-        obj.data.state = 2; 
-
-        // 2. 修改过期时间 (2099年)
-        obj.data.vipToTimeStr = "2099-09-09 09:09:09";
-        obj.data.vipToTime = "Wed Sep 09 09:09:09 CST 2099";
-
-        // 3. 修改金币/豆 (可选，数字类型)
-        obj.data.totalCoin = "99999"; 
-        obj.data.userDou = "9999";
-        
-        // 4. 修改显示信息 (可选)
-        obj.data.nickName = "Luis";
-        
-        // 5. 强制不需要手机号 (防止弹窗)
-        obj.data.needPhoneNumber = false;
-        
-        // --------------------------------------------
-        
-        console.log("皮皮学脚本执行成功：状态已改为 2，时间已延长");
-    } else {
-        console.log("皮皮学脚本执行警告：未找到 obj.data 字段");
-    }
-
-    $done({body: JSON.stringify(obj)});
-
-} catch (e) {
-    // 如果脚本报错，打印日志并返回原始数据，防止 App 崩溃
-    console.log("皮皮学脚本错误: " + e);
-    $done({});
+if (obj.data) {
+    // 固定设置为 2099 年 9 月 9 日
+    obj.data.vipToTimeStr = "2099-09-09 09:09:09";
+    
+    // 对应 Java Date.toString() 格式: Wed Sep 09 09:09:09 CST 2099
+    obj.data.vipToTime = "Wed Sep 09 09:09:09 CST 2099";
+    
+    // 附加修改：昵称和状态
+    obj.data.nickName = "IMPWELL";
+    obj.data.state = 2;
 }
+
+$done({body: JSON.stringify(obj)});
